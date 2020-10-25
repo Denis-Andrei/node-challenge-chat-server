3ea74ecca123a4448893a9f689c80486358b63bd
Denis-Andrei <denispeptanariu@gmail.com>
2020-10-25 19:05:39 +0000

    Level 1 DONE

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +21,37 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", function (req, res) {
+  const data = req.body; 
+  data.id = messages.length;
+  messages.push(data);
+  res.send('hello ' + data.from)
+});
+
+app.get("/messages", function (req, res) {
+    res.send(messages);
+});
+
+app.get("/messages/:id", function (req, res) {
+    const {id} = req.params;
+    let filteredMessages = messages.filter(message => message.id == id);
+    res.send(filteredMessages);
+});
+app.delete("/messages/:id", function (req, res) {
+    const {id} = req.params;
+    messages.forEach((item,index) => {
+      if(id == item.id){
+        messages.splice(index,1);
+        res.send(`Message with the id ${id} was deleted`);
+      }
+    })
+   
+    
+    
+});
+
+app.listen(3000);
